4b1ce3f66fb6f738a5196ad833b5ae519f75bd0a
Jawad TP <jawadtp248@gmail.com>
2022-10-15 23:01:03 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+//    Task 1:
+//    Create a file Main.java and RWOnly.java in same package
+//    Task 2:
+//    create a class named RWOnly in RWOnly.java, with a private member variable name
+//    Task 3:
+public static void main(String[] args) {
+    RWOnly obj=new RWOnly();
+    obj.setName("name");
+
+    System.out.println(obj.getName());
+    //java: name has private access in com.driver.RWOnly
+}
+//    create main function in Main.java, and crete an obj of class RWOnly in this file
+//    Task 4:
+//            try, setting a value to name, by directly accessing it using obj and also try printing it
+//    Error: you might seen some error, please not it down in commnets
+//    Task 5:
+//            try implementing getter and setters member function in class RWOnly
+//Task 6:
+//    Now, try setting a value to name in Main function using setter function and access it's value using getter function
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+//    Task 2:
+//    create a class named RWOnly in RWOnly.java, with a private member variable name
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
